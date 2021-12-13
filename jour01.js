5004a215b98165576dfff4c62a07d4dc41be93e0
pauline-wim <linette.wim@hotmail.com>
2021-12-13 13:56:06 +0100

    exos 1-5

---
+++ jour01.js
@@ -0,0 +1,30 @@
+// 01 - Hello World
+
+console.log("Hello World!");
+
+// 02 - String
+
+let test = "My name is Pauline";
+
+console.log(test);
+
+// 03 - Concatenation
+
+let name = "Pauline";
+
+console.log(`Nice to meet you ${name}`);
+
+// 04 - String Length
+
+let testLength = "I'm a very long !";
+
+console.log(testLength.length);
+
+// 05 - Replace
+
+let food = "croissant is meh";
+food = food.replace("meh", "so good");
+
+console.log(food);
+
+// 06 - Up and Down
