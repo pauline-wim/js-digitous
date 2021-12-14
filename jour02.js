13d0065843f353a678ed09ef0434510c0d018fd6
pauline-wim <linette.wim@hotmail.com>
2021-12-14 14:47:24 +0100

    Exos Bonus

--- jour02.js
+++ jour02.js
@@ -76,3 +76,65 @@ if (score >= limit && password.length > 5) {
 } else {
     console.log("Nothing is good");
 }
+
+// BONUS I
+
+let min = 1;
+let max = 6;
+
+let random = Math.floor(Math.random() * (max - min + 1) + 1);
+
+console.log(random);
+
+if (random === 6) {
+    console.log("Yes I win !");
+} else {
+    console.log("So close...");
+}
+
+// Bonus II
+
+let month = "January";
+
+switch (month) {
+    case "January":
+        console.log("Winter");
+        break;
+    case "February":
+        console.log("Winter");
+        break;
+    case "March":
+        console.log("Spring");
+        break;
+    case "April":
+        console.log("Spring");
+        break;
+    case "May":
+        console.log("Spring");
+        break;
+    case "June":
+        console.log("Summer");
+        break;
+    case "July":
+        console.log("Summer");
+        break;
+    case "August":
+        console.log("Summer");
+        break;
+    case "September":
+        console.log("Fall");
+        break;
+    case "October":
+        console.log("Fall");
+        break;
+    case "November":
+        console.log("Fall");
+        break;
+    case "December":
+        console.log("Winter");
+        break;
+    default: 
+		console.log("What month are we ?");
+}
+
+// Bonus III
