e1e0032d2788bfc49e7695ed75a7befbdab1b677
pauline-wim <linette.wim@hotmail.com>
2021-12-15 16:44:14 +0100

    bonus3

---
+++ jour03/bonus3.js
@@ -0,0 +1,5 @@
+const promo6 = ["Lysiane", "Jessica", "Ha-Chi", "Andrei", "Anita", "Chris", "Adrien", "Nicolas", "Charles", "Kévin", "Jean-Léonard", "Djibril", "Axel", "Caroline", "David", "Pauline"]
+
+let n = Math.floor(Math.random() * ((promo6.length - 1) + 1));
+
+console.log(promo6[n]);
