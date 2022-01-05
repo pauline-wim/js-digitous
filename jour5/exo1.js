3c7419a606b9d0c7c5d43c9d8264c4b63c711b75
pauline-wim <linette.wim@hotmail.com>
2022-01-05 14:42:40 +0100

    jour05

---
+++ jour5/exo1.js
@@ -0,0 +1,24 @@
+// 01 - Calculator
+
+function calculate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return num1 + num2;
+            break;
+        case "-":
+            return num1 - num2;
+            break;
+        case "x":
+            return num1 * num2;
+            break;
+        case "/":
+            return num1 / num2;
+            break;
+        case "%":
+            return num1 % num2;
+            break;
+    }
+}
+
+console.log(calculate(5, 4, "x"));
+
