e1471679647ad8edf356be057da4c407a739163b
pauline-wim <linette.wim@hotmail.com>
2022-01-10 13:43:29 +0100

    jour07/exo1

--- jour07/exo1.js
+++ jour07/exo1.js
@@ -1,1 +1,12 @@
 // 01 - File System
+
+const fs = require("fs");
+
+fs.readFile("./jour07/jour07.txt", function (err, data) {
+    if (err) {
+       console.error(err);
+       return;
+    }
+ 
+    console.log(data.toString());
+ });
