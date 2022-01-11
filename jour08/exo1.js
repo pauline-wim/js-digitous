9fbb8aea35a7a16ce0203fd9f12ce2033392afe4
pauline-wim <linette.wim@hotmail.com>
2022-01-11 14:29:31 +0100

    exo1.js

--- jour08/exo1.js
+++ jour08/exo1.js
@@ -6,8 +6,7 @@ let countriesNames = [];
 
 function getCountries() {
     axios.get("https://restcountries.com/v3.1/all").then(function (res) {
-        const countries = res.data;
-        const names = countries.filter(function (country) {
+        const countries = res.data.filter(function (country) {
             return countriesNames.push(country.name.common);
         })
         // console.log(res.data[0].name.common);
