bb3a61e9054fe481d4e515d4882b59bf168efa54
pauline-wim <linette.wim@hotmail.com>
2022-01-11 15:36:35 +0100

    898 random pokemon

--- jour08/exo3.js
+++ jour08/exo3.js
@@ -3,11 +3,11 @@
 const axios = require("axios");
 
 function catchPokemon(id) {
-    axios.get(`https://pokeapi.co/api/v2/ability/${id}/`).then(function (res) {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`).then(function (res) {
         console.log({id: res.data.id, name: res.data.name});
         });
 }
 
-let n = Math.floor(Math.random() * ((267 - 1) + 1));
+let n = Math.floor(Math.random() * ((898 - 1) + 1));
 
 catchPokemon(n);
