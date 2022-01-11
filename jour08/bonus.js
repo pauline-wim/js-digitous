a5cae5bef83ee03d7a1d64b4570990a38315a64d
pauline-wim <linette.wim@hotmail.com>
2022-01-11 17:13:01 +0100

    BLACK JACK

--- jour08/bonus.js
+++ jour08/bonus.js
@@ -18,8 +18,15 @@ const prompt = require("prompt");
 
 prompt.start();
 
-let player = 0;
+let firstCard = Math.floor(Math.random() * 10) + 1;
+let hiddenCard = Math.floor(Math.random() * 10) + 1;
+
+let player = firstCard + hiddenCard;
 let bank = Math.floor(Math.random() * (21 - 16 + 1)) + 16;
+
+console.log("Dealing... You get two cards, one known and one unknown.");
+console.log("First card: unknown");
+console.log("Second card:", firstCard);
 
 function game() {
 
@@ -53,11 +60,15 @@ function game() {
 function endGame() {
     if (player > 21) {
         console.log(`Your score is of ${player}. You lose...`);
+        console.log(`The dealer's hand was of ${bank}.`);
     } else if (player <= bank) {
         console.log(`Your score is of ${player}. You lose...`);
+        console.log(`The dealer's hand was of ${bank}.`);
     } else if (player = 21 || player > bank) {
-        console.log(`BLACK JACK! Your score is of ${player}.`);
+        console.log(`BLACK JACK!`);
         console.log("You win! Congrats, you lucky bastard!");
+        console.log(`Your score: ${player}.`);
+        console.log(`The dealer's hand: ${bank}.`);
     }
 };
 
